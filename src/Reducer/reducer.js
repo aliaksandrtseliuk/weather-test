5ef834ec1c574c527bd988383b72848f8ff3bc44
ALI <alex.ts.web@yandex.ru>
2020-10-24 09:06:22 +0300

    feat: add initial state / action types

--- src/Reducer/reducer.js
+++ src/Reducer/reducer.js
@@ -1,7 +1,39 @@
-import { START_LOADING, STOP_LOADING } from "../Actions/actionTypes";
+import {
+  START_LOADING,
+  STOP_LOADING,
+  SET_CURRENT_COORS,
+  SET_CURRENT_WEATHER_INFO,
+  SET_CURRENT_TIME,
+} from "../Actions/actionTypes";
 
 const initialState = {
   loading: false,
+  coors: {
+    lat: 53.13,
+    lon: 26.02,
+  },
+  info: {
+    currentInfo: {},
+    forecast: {
+      day_1st: {
+        day: "",
+        icon: "",
+        temp: "",
+      },
+      day_2nd: {
+        day: "",
+        icon: "",
+        temp: "",
+      },
+      day_3rd: {
+        day: "",
+        icon: "",
+        temp: "",
+      },
+    },
+    imageUrl: "",
+  },
+  currentTime: "",
 };
 
 const reducer = (state = initialState, action) => {
@@ -16,7 +48,21 @@ const reducer = (state = initialState, action) => {
         ...state,
         loading: false,
       };
-
+    case SET_CURRENT_COORS:
+      return {
+        ...state,
+        coors: action.coors,
+      };
+    case SET_CURRENT_WEATHER_INFO:
+      return {
+        ...state,
+        info: action.info,
+      };
+    case SET_CURRENT_TIME:
+      return {
+        ...state,
+        currentTime: action.time,
+      };
     default:
       return {
         ...state,
