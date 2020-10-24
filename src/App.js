cd500c18eee5f47be67195e3b1d99dc77c8d838b
ALI <alex.ts.web@yandex.ru>
2020-10-24 09:14:14 +0300

    feat: add content logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import classes from "./App.module.scss";
 import "antd/dist/antd.css";
-
 import Header from "./Components/Header/Header";
+import Content from "./Components/Content/Content";
+import Background from "./Components/Background/Background";
 
 const App = () => {
   return (
     <div className={classes.App}>
+      <Background />
       <Header />
+      <Content />
     </div>
   );
 };
